bad380a2ae059a4b14f2cb6e2f9babf7ac1ef542
tmltmltml <z5y3d6@sina.com>
2016-08-26 01:31:58 +0800

    1_version
    whether to optimize

---
+++ fun.cpp
@@ -0,0 +1,21 @@
+#include <cmath>                                     //mathematica lib
+//#include <iostream>                                  //standard input and output lib
+double fun(double x)
+{
+	double y = 4 * x * x * x - 8 * x * x + 2 * x + 1;
+	return y;
+}
+
+//eg:
+//C++              math
+//exp(x)           e ^ x;
+//pow(x, *)        x ^ *;
+//cos(x)           cosx;
+//sin(x)           sinx;
+//acos(x)          arccosx;
+//asin(x)          arcsinx;
+//atan(x)          arctanx;
+//fabs(x)          |x|;
+//log(x)           lnx;
+//log10(x)         lgx;
+//sqrt(x)          x ^ (1 / 2);
